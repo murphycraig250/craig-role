e2a0341281e7571b311b4c6c723378f477a9afdb
Craig Murphy <murphycraig250@gmail.com>
2026-02-21 19:46:03 +0000

    update linux role

--- manifests/linux_base.pp
+++ manifests/linux_base.pp
@@ -11,13 +11,4 @@ class role::linux_base {
   include profile::linux_user
   include profile::linux_cron
   include profile::linux_packages
-  if lookup('profile::linux_nfsserver::enabled', Boolean, 'first', false) {
-    include profile::linux_nfsserver
-  }
-  if lookup('profile::linux_nfsclient::enabled', Boolean, 'first', false) {
-    include profile::linux_nfsclient
-  }
-  if lookup('profile::linux_nginx::enabled', Boolean, 'first', false) {
-    include profile::linux_nginx
-  }
 }
