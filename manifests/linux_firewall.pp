b54f08e7792024879f05cba3be5a6b4423689dd2
craig <murphycraig250@gmai.com>
2026-03-16 19:27:11 +0000

    Auto commit

--- manifests/linux_firewall.pp
+++ manifests/linux_firewall.pp
@@ -12,29 +12,26 @@ class role::linux_firewall {
   -> Class['profile::linux_firewall::rules']
   -> Class['profile::linux_firewall::post']
 
-  # The Global Purge
-  resources { 'firewall':
+  firewallchain { 'INPUT:filter:IPv4':
     purge => true,
   }
 
-  # 1. Protect the Docker chain definitions
-  firewallchain { [
-      'DOCKER:filter:IPv4',
-      'DOCKER-BRIDGE:filter:IPv4',
-      'DOCKER-CT:filter:IPv4',
-      'DOCKER-FORWARD:filter:IPv4',
-      'DOCKER-INTERNAL:filter:IPv4',
-      'DOCKER-USER:filter:IPv4',
-    ]:
-      purge => false,
-  }
-
-  # 2. Tell the FORWARD chain specifically what to ignore
+# 2. Manage the FORWARD chain but ignore Docker's jumps
   firewallchain { 'FORWARD:filter:IPv4':
     purge  => true,
     ignore => [
       'DOCKER-USER',
       'DOCKER-FORWARD',
+      'DOCKER-ISOLATION', # Added this just in case
     ],
   }
-}
+
+# 3. Explicitly tell Puppet to leave the NAT table alone
+  firewallchain { [
+      'PREROUTING:nat:IPv4',
+      'POSTROUTING:nat:IPv4',
+      'OUTPUT:nat:IPv4',
+      'DOCKER:nat:IPv4',
+    ]:
+      purge => false,
+  }
