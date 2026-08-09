d0c2c162819b98d5d0e173fd5733bcf28faa9b7f
craig <murphycraig250@gmai.com>
2026-08-09 17:53:05 +0100

    Auto commit

--- manifests/windows_dc.pp
+++ manifests/windows_dc.pp
@@ -10,10 +10,10 @@ class role::windows_dc {
   include profile::dc_features
   include profile::dc_promotion
   include profile::dc_network
-}
 
-Class['profile::choco_setup']
--> Class['profile::choco_windows']
--> Class['profile::dc_network']
--> Class['profile::dc_features']
--> Class['profile::dc_promotion']
+  Class['profile::choco_setup']
+  -> Class['profile::choco_windows']
+  -> Class['profile::dc_network']
+  -> Class['profile::dc_features']
+  -> Class['profile::dc_promotion']
+}
